0efa63e2719da937b74c85af29e4e77605c75d16
hliang <jxhczhl@qq.com>
2019-08-17 18:08:17 +0800

    test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import requests
 import re
+#好饿啊，什么时候开饭
 class test:
     def __init__(self):
         print("初始化jxhczzz")
@@ -17,4 +18,5 @@ a=test()
 html=a.getHtmlText("http://www.baidu.com")
 resultlist=re.findAll(r'\d{3}-\d{7,8}(-\d{\d}?)')
 reslt=a.resultList(resultlist)
+print("2019-08-17")
 
